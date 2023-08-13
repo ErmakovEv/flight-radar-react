db1d833f957c4b503c49cb6084185c14b36e769a
ErmakovEv <ej-88@ya.ru>
2023-08-13 20:04:46 +0300

    fix:endpoint login deploy

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -2,7 +2,7 @@ const HOST = `https://serverflightradar.onrender.com`;
 
 const Endpoints = {
   AUTH: {
-    LOGIN: `${HOST}api/user/login`,
+    LOGIN: `${HOST}/api/user/login`,
     REFRECH: `${HOST}/api/user/refresh`,
     LOGOUT: `${HOST}/api/user/logout`,
     PROFILE: `${HOST}/api/setting/get-config`,
